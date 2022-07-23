52f0c5e5dab2cf88829d7b1f414caf9335a15308
kudal2 <dante._15@hotmail.com>
2022-07-23 02:11:59 -0400

    pruebados

--- places.js
+++ places.js
@@ -27,10 +27,10 @@ function renderPlaces(places) {
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        model.setAttribute('gltf-model', 'assets/asset.gltf');
-       model.setAttribute('rotation', '0 0 0');
-       model.setAttribute("position", "4 1 0");
+       model.setAttribute('rotation', '0 180 0');
+       model.setAttribute("position", "4 0.1 0");
        model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '1 1 1');
+       model.setAttribute('scale', '4 4 4');
        
 
        model.addEventListener('loaded', () => {
