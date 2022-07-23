c43cc6434963b5eb86b360563b5867dcf34fb3d6
kudal2 <dante._15@hotmail.com>
2022-07-23 01:57:12 -0400

    pruebauno

---
+++ places.js
@@ -0,0 +1,42 @@
+window.onload = () => {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+};
+
+function staticLoadPlaces() {
+   return [
+       {
+           name: 'asset',
+           location: {
+               lat: 10.243824601912154,
+               lng: -68.01064217093881,
+           }
+       },
+
+       
+   ];
+}
+
+function renderPlaces(places) {
+   let scene = document.querySelector('a-scene');
+
+   places.forEach((place) => {
+       let latitude = place.location.lat;
+       let longitude = place.location.lng;
+
+       let model = document.createElement('a-entity');
+       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gltf-model', 'assets/asset.gltf');
+       model.setAttribute('rotation', '0 0 0');
+       model.setAttribute("position", "4 1 0");
+       model.setAttribute('animation-mixer', '');
+       model.setAttribute('scale', '1 1 1');
+       
+
+       model.addEventListener('loaded', () => {
+           window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+       });
+
+       scene.appendChild(model);
+   });
+}
