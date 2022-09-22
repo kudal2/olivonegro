2f8b4744d920bbe1f8a7c18a9f31830072921a12
kudal2 <dante._15@hotmail.com>
2022-09-22 19:58:13 -0400

    conejos

--- places.js
+++ places.js
@@ -6,7 +6,7 @@ window.onload = () => {
 function staticLoadPlaces() {
    return [
        {
-           name: 'asset',
+           name: 'conejo',
            location: {
                lat: 7.7681343,
                lng: -72.2321226,
@@ -26,7 +26,7 @@ function renderPlaces(places) {
 
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: 10.24367679; longitude: -68.0106153;`);
-       model.setAttribute('gltf-model', 'assets/asset.gltf');
+       model.setAttribute('gltf-model', 'assets/conejo.gltf');
        model.setAttribute('rotation', '0 270 0');
        model.setAttribute("position", "9 0.1 0");
        model.setAttribute('animation-mixer', '');
