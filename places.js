a732754167d7ab5947f44c7e36cc59bab563d9a7
kudal2 <dante._15@hotmail.com>
2022-09-24 12:18:21 -0400

    conejopequeño

--- places.js
+++ places.js
@@ -27,10 +27,10 @@ function renderPlaces(places) {
        let model = document.createElement('a-entity');
        model.setAttribute('gps-entity-place', `latitude: 10.24367679; longitude: -68.0106153;`);
        model.setAttribute('gltf-model', 'assets/conejo.gltf');
-       model.setAttribute('rotation', '0 0 0');
+       model.setAttribute('rotation', '0 270 0');
        model.setAttribute("position", "9 0.1 0");
        model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '.5 .5 .5');
+       model.setAttribute('scale', '.3 .3 .3');
        
 
        model.addEventListener('loaded', () => {
